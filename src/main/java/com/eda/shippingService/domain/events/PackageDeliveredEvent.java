153152cbe3af320902379192d187aae16ed9a1ba
Jannik Alexander <jannik.alexander@innovation-hub.de>
2024-10-25 10:52:42 +0200

    chore: optimize imports

--- src/main/java/com/eda/shippingService/domain/events/PackageDeliveredEvent.java
+++ src/main/java/com/eda/shippingService/domain/events/PackageDeliveredEvent.java
@@ -3,13 +3,7 @@ package com.eda.shippingService.domain.events;
 import com.eda.shippingService.domain.dto.outgoing.AddressDTO;
 import com.eda.shippingService.domain.entity.Shipment;
 import com.eda.shippingService.domain.events.common.DomainEvent;
-import com.eda.shippingService.domain.events.common.Message;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import elemental.json.JsonString;
-import elemental.json.impl.JreJsonString;
 import lombok.Getter;
-import org.springframework.lang.Nullable;
 
 import java.util.UUID;
 
--- src/main/java/com/eda/shippingService/domain/events/PackageSentEvent.java
+++ src/main/java/com/eda/shippingService/domain/events/PackageSentEvent.java
@@ -2,11 +2,7 @@ package com.eda.shippingService.domain.events;
 
 import com.eda.shippingService.domain.dto.outgoing.PackageDTO;
 import com.eda.shippingService.domain.entity.APackage;
-import com.eda.shippingService.domain.entity.Shipment;
 import com.eda.shippingService.domain.events.common.DomainEvent;
-import com.eda.shippingService.domain.events.common.Message;
-
-import java.util.UUID;
 
 public class PackageSentEvent extends DomainEvent<PackageDTO> {
     public PackageSentEvent(APackage aPackage) {
