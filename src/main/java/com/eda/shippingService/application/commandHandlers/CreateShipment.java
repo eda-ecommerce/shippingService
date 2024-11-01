2cb28481dc2e3698de6da856fafdb916a43b2b47
Jannik <jannik.alexander@innovation-hub.de>
2024-11-01 20:07:29 +0100

    chore: rename shipment to shipmentDTO

--- src/main/java/com/eda/shippingService/application/commandHandlers/CreateShipment.java
+++ src/main/java/com/eda/shippingService/application/commandHandlers/CreateShipment.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//The Problem with this Handler / Use case pattern:
+//Every time I call this from a different location, this has to be adjusted to process different ways of calling handle.
+//OR: I conform to the CreateShipmentRequestDTO and adjust the calling code to fit this pattern.
 public class CreateShipment {
 
     private final ShipmentRepository shipmentRepository;
@@ -19,12 +22,11 @@ public class CreateShipment {
         this.reserveStock = reserveStock;
     }
 
-    public Shipment handle(CreateShipmentRequestDTO shipment) {
-        if (shipment.orderId() != null && shipmentRepository.findByOrderId(shipment.orderId()) != null) {
-            throw new IllegalArgumentException(String.format("Shipment with Order ID %s already exists.", shipment.orderId()));
+    public Shipment handle(CreateShipmentRequestDTO shipmentDTO) {
+        if (shipmentDTO.orderId() != null && shipmentRepository.findByOrderId(shipmentDTO.orderId()) != null) {
+            throw new IllegalArgumentException(String.format("Shipment with Order ID %s already exists.", shipmentDTO.orderId()));
         }
-
-        Shipment shipmentEntity = shipment.toEntity();
+        Shipment shipmentEntity = shipmentDTO.toEntity();
 
         if (shipmentEntity.getStatus() == ShipmentStatus.REQUESTED) {
             // instead of directly decreasing the stock here, we are just reserving the necessary stock amount
