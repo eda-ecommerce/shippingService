1c3b6bb3614ed019d32018a0568cd08040a6a49c
Jannik Alexander <jannik.alexander@innovation-hub.de>
2024-11-11 18:42:37 +0100

    refactor: [Business Logic] OrderRequested now actually triggers creation of a Shipment

--- src/main/java/com/eda/shippingService/application/eventHandlers/OrderRequestedEventHandler.java
+++ src/main/java/com/eda/shippingService/application/eventHandlers/OrderRequestedEventHandler.java
@@ -1,11 +1,11 @@
 package com.eda.shippingService.application.eventHandlers;
 
 import com.eda.shippingService.application.service.ShipmentService;
-import com.eda.shippingService.application.service.StockService;
+import com.eda.shippingService.application.service.StockServiceImpl;
 import com.eda.shippingService.domain.dto.incoming.RequestShipmentDTO;
 import com.eda.shippingService.domain.dto.outgoing.OrderLineItemDTO;
 import com.eda.shippingService.domain.events.OrderRequested;
-import com.eda.shippingService.infrastructure.eventing.EventPublisher;
+import com.eda.shippingService.adapters.eventing.EventPublisher;
 import com.eda.shippingService.infrastructure.repo.IdempotentConsumerRepository;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
@@ -17,15 +17,11 @@ import org.springframework.stereotype.Component;
 public class OrderRequestedEventHandler implements EventHandler<OrderRequested> {
     private final IdempotentConsumerRepository idempotentConsumerRepository;
     private final ShipmentService shipmentService;
-    private final EventPublisher eventPublisher;
-    private final StockService stockservice;
 
     @Autowired
-    public OrderRequestedEventHandler(IdempotentConsumerRepository idempotentConsumerRepository, ShipmentService shipmentService, EventPublisher eventPublisher, StockService stockservice) {
+    public OrderRequestedEventHandler(IdempotentConsumerRepository idempotentConsumerRepository, ShipmentService shipmentService) {
         this.idempotentConsumerRepository = idempotentConsumerRepository;
         this.shipmentService = shipmentService;
-        this.eventPublisher = eventPublisher;
-        this.stockservice = stockservice;
     }
 
     @Transactional
@@ -40,7 +36,7 @@ public class OrderRequestedEventHandler implements EventHandler<OrderRequested> 
         var created = shipmentService.requestShipment(new RequestShipmentDTO(
                 event.getPayload().orderId(),
                 event.getPayload().customerId(),
-                //TODO Where do the addresses come from?
+                //TODO Where do the addresses come from? --> PrepareShipment, but via controller?
                 null,null,
                 event.getPayload().products().stream().map(product -> new OrderLineItemDTO(product.productId(), product.quantity())).toList()));
         var requestedProducts = created.getRequestedProducts();
