39670b50b92c11c2a028c30d16b049449403aa79
Jannik Alexander <jannik.alexander@innovation-hub.de>
2024-11-12 16:35:31 +0100

    chore: RuntimeException does not make compiler mad

--- src/main/java/com/eda/shippingService/application/service/exception/IncompleteContentException.java
+++ src/main/java/com/eda/shippingService/application/service/exception/IncompleteContentException.java
@@ -1,6 +1,6 @@
 package com.eda.shippingService.application.service.exception;
 
-public class IncompleteContentException extends Exception {
+public class IncompleteContentException extends RuntimeException {
     public IncompleteContentException(String msg){
         super(msg);
     }
